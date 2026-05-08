189c0cc7660b9f1bd95d5fa9ad08437fd4b82556
Ubuntu <ubuntu@ip-172-31-181-126.ec2.internal>
2026-05-08 08:56:14 +0000

    Updated homepage text to test CI/CD flow

--- app/app.py
+++ app/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "DevOps Assignment Running Successfully"
+    # Changed text to verify the CI/CD pipeline works
+    return "DevOps Assignment - CI/CD Pipeline Tested Successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
